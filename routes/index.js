df107f4501942784fe59b274ed1f158466420119
Nikhil5037 <nduvvuru@my.centennialcollege.ca>
2021-02-14 01:02:23 +0530

    servises changed

--- routes/index.js
+++ routes/index.js
@@ -19,18 +19,25 @@ router.get('/projects', function(req, res, next) {
   res.render('projects',{ title: "projects",
      AllProjects: [
       {
-        Title: "smartdhyana",
+        Title: "SMARTDHYANA",
         Role: "Developer",
         WebAddress: "https://www.smartdhyana.com/",
         img:"https://smartdhyanablog.files.wordpress.com/2019/08/dhyana_-why-do-you-need-a-device-for-meditation.png",
-        description: "Dhyana is a Meditation Ring which Tracks your HRV Values and also calculate your Mindfulnes. A Ring that can Test Your Meditaion and Gives How stresful, Relaxed you are throgh the Meditaion sessions"
+        description: "Dhyana is a Meditation Ring which tracks your HRV Values and also calculates your attention. A ring that can test your Meditaion and gives how stressful and relaxed you are through out the Meditaion sessions"
       },
       {
         Title: "BankMuscat InternetBanking",
         Role: "Performance Tester",
         WebAddress: "https://www.bankmuscatonline.com/",
         img:"/Assets/BM.png",
-        description: "Bank Muscat is a leading bank in Oman. Internet Banking Application for which different types of load testing is done as per the Requirements."
+        description: "Bank Muscat is a financial services provider in the Sultanate of Oman providing corporate banking, retail banking, investment banking, treasury, private banking and asset management. Internet Banking application enables customers to conduct a range of financial transactions through the financial institution's website."
+      },
+      {
+        Title: "Commercial Bank International",
+        Role: "Performance Tester",
+        WebAddress: "https://www.cbiuae.com/",
+        img:"/Assets/CBI.jpg",
+        description: "CBI is a leading Finance provider in U.A.E. CBI offers a wide range of banking products & services to suit your financial needs."
       },
     ] }
   );
